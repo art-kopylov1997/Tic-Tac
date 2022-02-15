52ae85f692e8e0879f47326b0a310852e472b48b
art-kopylov1997 <art.kopylov1997@mail.ru>
2022-02-15 21:32:36 +0300

    first commit

---
+++ src/components/Board.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Board.css'
+import Square from "./Square";
+
+const Board = ({squares, click}) => {
+    return (
+        <div className="board">
+            {
+                squares.map((square, index) => (
+                    <Square key={index} value={square} onClick={() => click(index)} />
+                ))
+            }
+        </div>
+    );
+}
+
+export default Board;
